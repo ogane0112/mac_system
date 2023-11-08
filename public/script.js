3c6335a08e03129f53a53f42ad4341cd2660388c
ogane0112 <kai513272@gmail.com>
2023-11-08 14:21:07 +0900

    実際に使用した物です
    機能として多くの注文があると画面の表示が見えづらくなるので2分で注文完了の表示は消えるように修正いたしました！

--- public/script.js
+++ public/script.js
@@ -1,34 +1,62 @@
+const removedOrders = [];
+
 async function fetchOrders() {
     const response = await fetch('/orders');
     const data = await response.json();
-    
+
     const waitingOrders = document.getElementById('waitingOrders');
     const completedOrders = document.getElementById('completedOrders');
-  
-    // 一旦クリア
-    waitingOrders.innerHTML = '<h2>Waiting Orders</h2>';
-    completedOrders.innerHTML = '<h2>Completed Orders</h2>';
-  
-    data.forEach(orderRow => {
-      const orderId = orderRow[0];
-      const status = orderRow[3]; // 4列目
-  
-      const orderDiv = document.createElement('div');
-      orderDiv.innerText = `Order ID: ${orderId}`;
-  
-      if (status === '注文完了') {
-        completedOrders.appendChild(orderDiv);
-      } else {
-        waitingOrders.appendChild(orderDiv);
-      }
-    });
 
-  }
-  function goFullScreen() {
-  const elem = document.documentElement;
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  }
+    data.forEach(orderRow => {
+        const orderId = orderRow[1];
+        const ordernum = orderRow[0];
+        const status = orderRow[5];
+
+        if (removedOrders.includes(ordernum)) {
+            return;
+        }
+
+        const time = new Date(orderRow[4]);
+        const now = new Date();
+        const check = (now - time) / (60 * 1000);
+
+        const existingOrder = document.getElementById(`order-num-${ordernum}`);
+
+        const removeOrderAfterDelay = (orderElement) => {
+            setTimeout(function() {
+                if (completedOrders.contains(orderElement)) {
+                    completedOrders.removeChild(orderElement);
+                    removedOrders.push(ordernum);
+                }
+            }, 120000);
+        };
+
+        if (existingOrder) {
+            existingOrder.innerText = `整理券: ${orderId} 会計番号: ${ordernum}`;
+
+            if (status === '注文完了' && check <= 15 && !completedOrders.contains(existingOrder)) {
+                waitingOrders.removeChild(existingOrder);
+                completedOrders.appendChild(existingOrder);
+                removeOrderAfterDelay(existingOrder);  // ここにタイマーを追加
+            } else if (status !== "注文完了" && check <= 15 && !waitingOrders.contains(existingOrder)) {
+                completedOrders.removeChild(existingOrder);
+                waitingOrders.appendChild(existingOrder);
+            }
+
+        } else {
+            const orderDiv = document.createElement('div');
+            orderDiv.id = `order-num-${ordernum}`;
+            orderDiv.innerText = `整理券: ${orderId} 会計番号: ${ordernum}`;
+
+            if (status === '注文完了' && check <= 15) {
+                completedOrders.appendChild(orderDiv);
+                removeOrderAfterDelay(orderDiv);
+            } else if (status !== "注文完了" && check <= 15) {
+                waitingOrders.appendChild(orderDiv);
+            }
+        }
+    });
 }
-  setInterval(fetchOrders, 10000); // 10秒ごとに更新
-  
+
+setInterval(fetchOrders, 10000);
+
--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const port = 3000;
 let uniqueOrders = [];
 
 async function fetchOrders() {
-  const apiKey = '';
-  const sheetId = '';
-  const sheetName = 'sheet1';
+
+  const apiKey = 'スプレッドシートのapiキーを入力してください!';
+  const sheetId = 'スプレッドシートのidを入力してください';
+  const sheetName = '取得したいスプレッドシートのシート名を入力してください';
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`;
 
   try {
@@ -18,22 +19,30 @@ async function fetchOrders() {
     // 重複したorderIdを削除
     const orderMap = new Map();
     for (const order of orders) {
+
       const orderId = order[0];
+
       if (!orderMap.has(orderId)) {
+
         orderMap.set(orderId, order);
+
       }
     }
     uniqueOrders = Array.from(orderMap.values());
 
   } catch (error) {
+
     console.error('Error fetching orders:', error);
+
   }
 }
 
 setInterval(fetchOrders, 10000); // 10秒ごとに更新
 
 app.get('/orders', (req, res) => {
+
   res.json(uniqueOrders);
+  
 });
 
 app.use(express.static('public')); // 静的ファイルを提供
